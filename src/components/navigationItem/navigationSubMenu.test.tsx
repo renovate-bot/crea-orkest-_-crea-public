93a5bf200670d4ec0d200037ef8ccdf1948c2d32
thijsvandiessen <thijsvandiessen@gmail.com>
2023-09-19 02:49:57 +0200

    fix: feedback

--- src/components/navigationItem/navigationSubMenu.test.tsx
+++ src/components/navigationItem/navigationSubMenu.test.tsx
@@ -3,10 +3,12 @@ import { render, screen } from '@testing-library/react'
 
 describe('NavigationItem', () => {
   it('should render a link', async () => {
-    render(<NavigationItem slug={'url'} label={'homepage'} />)
+    const { container } = render(
+      <NavigationItem slug={'url'} label={'homepage'} />
+    )
 
-    expect(screen.getByText('homepage'))
-    expect(screen.getByRole('link', { name: 'homepage' })).toMatchSnapshot()
+    expect(screen.getByRole('link', { name: 'homepage' }))
+    expect(container).toMatchSnapshot()
   })
 
   it('should render a link warning', async () => {
